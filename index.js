e21a397834f16c22d7ef12e9a1da61f800bfbf90
Juan Felipe Ramirez Bolivar <67343079+Felipe11114@users.noreply.github.com>
2021-05-07 14:16:20 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const http = require('http');
-const port = process.env.PORT || 3000;
+
+const hostname = '127.0.0.1';
+const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola Mundo');
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+server.listen(port, hostname, () => {
+  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
 });
